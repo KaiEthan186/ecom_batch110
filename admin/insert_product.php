bd15214d645dc5e8547e8e874ad1adec277dfd0f
KaiEthan186 <maungthant186@gmail.com>
2025-08-01 15:07:11 +0630

    processing

--- admin/insert_product.php
+++ admin/insert_product.php
@@ -1,4 +1,5 @@
 <?php
+
     require_once "dbconnect.php";
     try{
         $sql="select * from category";
@@ -6,8 +7,37 @@
         $stmt->execute();
         $categories=$stmt->fetchAll();
     }catch(PDOException $e) {
-        echo"". $e->getMessage();
+        echo"". $e->getMessage();  
     }
+
+    if(isset($_POST["insertBtn"])){
+        $name=$_POST["pname"];
+        $price=$_POST["price"];
+        $category=$_POST["category"];
+        $qty=$_POST["qty"];
+        $description=$_POST["description"];
+        $fileImage=$_FILES["productImage"];
+
+        // echo $name. "<br>";
+        // echo $price. "<br>";
+        // echo $category. "<br>";
+        // echo $qty. "<br>";
+        // echo $description. "<br>";
+        // echo $fileImage['name']. "<br>";
+
+        $filePath="productImage/".$fileImage['name'];
+        //upload to a specified directory
+        $status= move_uploaded_file($fileImage['tmp_name'],$filePath);
+        if ($status){
+            echo"file uploaded";
+            echo"<img src=$filePath>";
+        }else{
+            echo"file upload fail";
+        }
+        
+    }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -22,40 +52,55 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-            <?php require_once("navbarcopy.php"); ?>
+            <?php require_once "navbarcopy.php";?>
         </div>
-        <div class="row mx-auto">
-            <div class="col-md-9">
-                <form class= "form" action="insert_product.php" method="post" enctype="multipart/form-data">
-                    <div class="col-md-4">
-                        <div class="mb-1">
-                            <label for="pname" class="form-label">Product Name</label>
-                            <input type="text" class="form-control" name="pname">
-                        </div>
-
-                        <div class="mb-1">
-                            <label for="price" class="form-label">Price</label>
-                            <input type="number" class="form-control" name="price">
-                        </div>
-
-                        <select name="category" class="form-select">
+        <div class="row mt-3">
+            <div class="col-md-12 mt-3 px-5">
+                <h5 class="text-center">Insert Product</h5>
+                <form class="form border border-dark rounded p-4" action="insert_product.php" method="post" enctype="multipart/form-data">
+                <div class="row px-5">
+                        <div class="col-md-5 mx-3 px-5">
+                            <div class="mb-1" bg-light>
+                                <label for="pname" class="form-label">Product Name</label>
+                                <input type="text" class="form-control" name="pname">
+                            </div>
+                            <div class="mb-1" bg-light>
+                                <label for="price" class="form-label">Price</label>
+                                <input type="number" class="form-control" name="price">
+                            </div>
+                            <select name="category" class="form-select bg-light">
                             <option value="">Choose Category</option>
-                            <?php 
-
-                            if(isset($categories)){
-                                foreach($categories as $category){
-                                    echo "<option value=$category[catID]>$category[catName]</option>";
-                            }
-                        
+                            <?php
+                            if (isset($categories)) {
+                                foreach ($categories as $category) {
+                                    echo "<option value=$category[catId]> $category[catName]</option>";
+                                }
                             }
                             ?>
-                        </select>
+                            </select>
+                        </div>
+                        <div class="col-md-5 mx-3 px-3">
+                            <div class="mb-1">
+                                <label class="form-label">Quantity</label>
+                                <input type="number" class="form-control" name="qty">
+                            </div>
+                            <div class="mb-1">
+                                <textarea name="description" id="" class="form-control">
+
+                                </textarea>
+                            </div>
+                            <div class="mb-1">
+                                <label for="">Choose Product Image</label>
+                                <input type="file" class="form-control" name="productImage">
+                            </div>
+
+                            <button type="submit" name="insertBtn" class="btn btn-primary rounded-pill mt-4">Insert Product</button>
+                        </div>
                     </div>
                 </form>
             </div>
         </div>
-
     </div>
-    
+
 </body>
 </html>
